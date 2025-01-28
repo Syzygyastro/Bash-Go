06606eaaf285bef0a823d144daa1e6f9075e1004
stormzy <stormzy6776@gmail.com>
2025-01-28 12:46:10 +0000

    [any message]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -9,17 +9,29 @@ import (
 
 func main() {
 	for {
+		set := map[string]bool{}
+
 		fmt.Fprint(os.Stdout, "$ ")
+
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
 		command = command[:len(command)-1] // Remove the newline character
+		fields := strings.Fields(command)
+
 		if command == "exit 0" {
 			os.Exit(0)
-		} else if strings.Fields(command)[0] == "echo" {
+		} else if fields[0] == "echo" {
 			fmt.Println(strings.Join(strings.Fields(command)[1:], " "))
+		} else if fields[0] == "type" {
+			if val, exists := set[fields[1]]; exists {
+				fmt.Println(val, "is a shell builtin")
+			} else {
+				fmt.Println(fields[1] + ": not found")
+			}
 		} else {
 			fmt.Println(command + ": command not found")
 		}
